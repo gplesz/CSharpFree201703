093345c1adacb2762672f1b4fb54ce1cfb9499ac
Plesz Gábor <gabor.plesz@gmail.com>
2017-03-07 10:40:23 +0100

    #1: Hello világ példaprogram

---
+++ CSharpAlapok/01Bevezetes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01Bevezetes
+{
+
+    //   Program(C#) -> IL (Intermediate Language=Köztes nyelv)
+    //
+    //                       \ IL /
+    //                       ------
+    //                       |.NET|        |.NET|         |.NET|     
+    //                       ------        ------         ------
+    //                       WINDOWS       LINUX          Mac OS      Android   iOS
+    //
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Sziasztok itt vagyok");
+            Console.WriteLine("Sziasztok itt vagyok");
+            Console.WriteLine("Sziasztok itt vagyok");
+            //Console.ReadLine();
+        }
+    }
+}
